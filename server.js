febe25dabca71b55502b66419e791c6398aed0f1
bronxpostma13 <bronxpostma@gmail.com>
2026-06-10 11:11:50 +0200

    begin pagina route + data ophalen #6

--- server.js
+++ server.js
@@ -18,6 +18,27 @@ app.engine('liquid', engine.express());
 app.set('views', './views')
 
 
+const pokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const pokemonResponseJSON = await pokemonResponse.json()
+   const pokemons = await Promise.all(
+    pokemonResponseJSON.results.map(async (entry) => {
+      const detailResponse = await fetch(entry.url)
+      const detail = await detailResponse.json()
+
+      return {
+        name: detail.name,
+        id: detail.id,
+        sprite: detail.sprites.other['official-artwork'].front_default,
+        types: detail.types.map(t => t.type.name)  // bijv. ['grass', 'poison']
+      }
+    })
+  )
+
+// Maak een GET route voor de index
+app.get('/', async function (request, response) {
+ 
+  response.render('index.liquid', { pokemons })
+})
 
 // Stel de poort in
 app.set('port', process.env.PORT || 8000)
