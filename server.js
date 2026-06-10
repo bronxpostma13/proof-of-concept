fb3d3a2b707681b295ef05333877efccb946c7c5
bronxpostma13 <bronxpostma@gmail.com>
2026-06-10 11:10:06 +0200

    Begin server code #5

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Importeer het npm package Express
+import express from 'express'
+
+// Importeer de Liquid package
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({extended: true}))
+
+// Gebruik de map 'public' voor statische bestanden
+app.use('/assets' , express.static('assets'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views')
+
+
+
+// Stel de poort in
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op
+app.listen(app.get('port'), function () {
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+// 404-pagina als de route niet bestaat
+app.use((request, response) => {
+    response.render("404.liquid");
+});
